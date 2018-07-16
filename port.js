470435d539ab89439b6d62c636d292c28cf8412f
George <george.romeroo93@gmail.com>
2018-07-16 18:54:13 -0400

    Best works!

---
+++ port.js
@@ -0,0 +1,13 @@
+var hoverboards = 0;
+if (hoverboards >= 100) {
+  console.log("The future rocks!");
+}
+  else if (hoverboards < 100 && hoverboards > 1) {
+    console.log("The future is pretty cool");
+  }
+  else if (hoverboards === 1) {
+    console.log("Meh");
+    }
+  else {
+    console.log("The future is boring.");
+  }
